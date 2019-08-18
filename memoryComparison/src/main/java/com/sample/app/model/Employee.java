da95e743ab52ccd0b8dbde2b9ca0a3efc26a9a87
Hari Krishna Gurram <hari.krishna.gurram@sap.com>
2019-08-18 11:13:16 +0530

    Check memory foot print between XML, JSON and Protocol Buffer

---
+++ memoryComparison/src/main/java/com/sample/app/model/Employee.java
@@ -0,0 +1,52 @@
+package com.sample.app.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "employee")
+public class Employee {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private Address address;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Employee [id=").append(id).append(", firstName=").append(firstName).append(", lastName=")
+				.append(lastName).append(", address=").append(address).append("]");
+		return builder.toString();
+	}
+
+}
